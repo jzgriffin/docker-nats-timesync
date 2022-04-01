27f7834875cd5a26fe43302a8c29817023f776fc
Jeremiah Griffin <jgriffin@mailbox.org>
2022-04-01 14:42:13 -0700

    Implement publisher program

--- publisher/src/main.rs
+++ publisher/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use chrono::{self, TimeZone, DateTime};
+use nats;
+
+const USEC_PER_SEC: u128 = 1_000_000u128;
+
+fn datetime_microseconds<Tz: TimeZone>(dt: DateTime<Tz>) -> u128 {
+    u128::try_from(dt.timestamp()).unwrap() * USEC_PER_SEC
+        + u128::from(dt.timestamp_subsec_micros())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let nats_url = "localhost";
+    let time_subject = "time.system";
+    let publish_interval = Duration::from_millis(500);
+
+    // The NATS server may not be fully initialized when this process starts.
+    // Ensure that we retry on failed connections, using the default nats.rs backoff.
+    let nats_options = nats::Options::new().retry_on_failed_connect();
+    println!("Connecting to NATS URL {}", nats_url);
+    let nats_client = nats_options.connect(nats_url)?;
+
+    loop {
+        // Publish the time as local microseconds since the epoch.
+        let now_us = datetime_microseconds(chrono::Local::now());
+        println!("Sending timestamp {}", now_us);
+        nats_client.publish(time_subject, now_us.to_le_bytes())?;
+
+        std::thread::sleep(publish_interval);
+    }
 }
